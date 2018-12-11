cf3589e634f22950d16dbc875c4e71e7127dff74
bondrogeen <bondrogeen@gmail.com>
2018-12-11 10:36:01 +0300

    fix

--- README.md
+++ README.md
@@ -12,17 +12,17 @@ http://192.168.1.49/DS18B20.lua?init=4
 
 > Search for all  1-Wire device. Return the number of sensors found
 
-http://192.168.1.49/DS18B20.lua?find=4
+http://192.168.1.49/DS18B20.lua?find=true
 
 > Attention! Before obtaining the temperature, if the sensors are not polled for a long time, you must first send the scan command
 
-http://192.168.1.49/DS18B20.lua?scan=1 
+http://192.168.1.49/DS18B20.lua?scan=true
 
 > Getting the temperature of the first sensor found
 
 http://192.168.1.49/DS18B20.lua?get=1 
 
-> Get the temperature of all found sensors. Returns an array of data  ( "124EAB300":"30.2500","FFF63116153":"32.5000","FFC73816153":"32.5625"} )
+> Get the temperature of all found sensors. Returns an object of data  ( "124EAB300":"30.2500","FFF63116153":"32.5000","FFC73816153":"32.5625"} )
 
 http://192.168.1.49/DS18B20.lua?get=all
 
@@ -35,13 +35,11 @@ pin = 4  -- gpio2
 
 dofile("DS18B20.lua")({init=pin}) -- Sets a pin in 1-Wire mode.
 
-addr = dofile("DS18B20.lua")({find=pin})  --Search for all  1-Wire device. Return the number of sensors found
-print(addr)
+print(dofile("DS18B20.lua")({find=true})) --Search for all  1-Wire device. Return the number of sensors found
 
 -- dofile("DS18B20.lua")({scan=1}) -- Command to all devices for temperature conversion
 
-r=dofile("DS18B20.lua")({get=1})  -- Getting the temperature of the first sensor found
-print(r)
+print(dofile("DS18B20.lua")({get=1})) -- Getting the temperature of the first sensor found
 
 -- or get the temperature of all found sensors
 
--- files/DS18B20.lua
+++ files/DS18B20.lua
@@ -1,4 +1,4 @@
-local p=_ds18b20 and _ds18b20.pin
+local p=_DS18B20 and _DS18B20.pin
 
 local function scan()
 local r=ow.reset(p)
@@ -8,15 +8,15 @@ return r==1
 end
 
 local function find()
-if not p then return end
+if not p then return false end
 ow.reset_search(p)
 local d,a={}
 repeat
  a = ow.search(p)
  if a then print(a:byte(1,8)) d[#d+1]=a end
 until not a
-if(#d>0)then _ds18b20.adr=d scan(p) end
-return #d
+if(#d>0)then _DS18B20.adr=d scan() end
+return #d or false
 end
 
 local function temp(x)
@@ -32,32 +32,30 @@ end
 return r
 end
 
-local function getTemp(x)
-if not _ds18b20 then return"first init 1-Wire" end
-if not _ds18b20.adr then return"find device" end
-local a,t,r=_ds18b20.adr,{}
-x=tonumber(x)and tonumber(x)or x
-if type(x)=="number"then
- if a and x<=#a then r=temp(_ds18b20.adr[x])else r="none"end
-else for i,v in ipairs(_ds18b20.adr)do t[string.format("%X%X%X%X%X%X", _ds18b20.adr[i]:byte(2,7))]=temp(v) end
- r=t
+local function get(x)
+if not _DS18B20 or not _DS18B20.adr then return false end
+local a,t,r=_DS18B20.adr,{}
+x=tonumber(x) or x
+if type(x)=="number"then r=(a[x]) and temp(_DS18B20.adr[x]) or false
+else for i,v in ipairs(_DS18B20.adr)do t[string.format("%X%X%X%X%X%X", _DS18B20.adr[i]:byte(2,7))]=temp(v) end
+r=t
 end
 scan()
 return r
 end
 
 local function init(p)
-_ds18b20={}
-_ds18b20.pin=p
+_DS18B20={}
+_DS18B20.pin=p
 ow.setup(p)
 return true
 end
 
 return function (t)
  local r
- if(t.init)then r=init(t.init) end
- if(t.find)then r=find() end
- if(t.scan)then r=scan() end
- if(t.get)then r=getTemp(t.get) end
+ if(t.init)then r=init(t.init)end
+ if(t.find)then r=find()end
+ if(t.scan)then r=scan()end
+ if(t.get)then r=get(t.get)end
  return r
 end
